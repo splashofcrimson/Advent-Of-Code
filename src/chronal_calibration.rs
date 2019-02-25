c718b456aed9541c85a262e78742d06b137e0f99
splashofcrimson <splashofcrimson@gmail.com>
2019-02-25 01:31:48 -0500

    :sparkles: finished day01

--- src/chronal_calibration.rs
+++ src/chronal_calibration.rs
@@ -1,8 +1,14 @@
-use std::fs;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
 
 pub fn run() {
-    let input = fs::read_to_string("../input/day01.txt").expect("Unable to read file");
-
-    println!("{:?}", input);
-
+    let buf = BufReader::new(File::open("input/day01.txt").unwrap());
+    let lines: Vec<String> = buf.lines()
+        .map(|l| l.unwrap())
+        .collect();
+    let int_lines: Vec<i32> = lines.iter()
+        .map(|v| v.parse::<i32>().unwrap())
+        .collect();
+    let sum : i32 = int_lines.iter().fold(0, |sum, val| sum + val);
+    println!("Resulting frequency: {}", sum);
 }
