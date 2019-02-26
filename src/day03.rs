ce1b3ef114e8bcc27ad75904d1477104a8585b8d
splashofcrimson <splashofcrimson@gmail.com>
2019-02-25 22:26:43 -0500

    :hammer: refactor Claim struct

--- src/day03.rs
+++ src/day03.rs
@@ -1,8 +1,9 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
-use std::collections::HashMap;
+use std::collections::{HashSet, HashMap};
 
 struct Claim {
+    id: String,
     x: i32,
     y: i32,
     width: i32,
@@ -38,6 +39,7 @@ pub fn five() {
             .collect();
 
         let c = Claim {
+            id: split_claim[0 as usize].clone(),
             x: coordinates[0 as usize].clone().parse().unwrap(),
             y: coordinates[1 as usize].clone().trim_matches(':').parse().unwrap(),
             width: bounds[0 as usize].clone().parse().unwrap(),
@@ -66,4 +68,56 @@ pub fn five() {
 
     }
     // println!("{}", square_inches);
+}
+
+pub fn six() {
+    let mut split_claim: Vec<String>;
+    let mut coordinates: Vec<String>;
+    let mut bounds: Vec<String>;
+
+    let mut claims = HashMap::new();
+    let mut claim_ids = HashSet
+
+    let buf = read_from_file();
+    for claim in &buf {
+        split_claim = claim.split(" ")
+            .map(|s| s.to_string())
+            .collect();
+        coordinates = split_claim[2 as usize].split(",")
+            .map(|s| s.to_string())
+            .collect();
+        bounds = split_claim[3 as usize].split("x")
+            .map(|s| s.to_string())
+            .collect();
+
+        let c = Claim {
+            id: split_claim[0 as usize].clone(),
+            x: coordinates[0 as usize].clone().parse().unwrap(),
+            y: coordinates[1 as usize].clone().trim_matches(':').parse().unwrap(),
+            width: bounds[0 as usize].clone().parse().unwrap(),
+            height: bounds[1 as usize].clone().parse().unwrap(),
+        };
+
+        for x in (c.x+1)..=(c.x+c.width) {
+            for y in (c.y+1)..=(c.y+c.height) {
+                let coordinate = (x, y);
+                if !claims.contains_key(&coordinate) {
+                    claims.insert(coordinate, );
+                }
+                else {
+                    match claims.get(&coordinate) {
+                        Some(&count) => {
+                            if count == 1 {
+                                claims.insert(coordinate, 2);
+                                square_inches += 1;
+                            }
+                        },
+                        _ => println!("ERROR"),
+                    }
+                }
+            }
+        }
+
+    }
+
 }
